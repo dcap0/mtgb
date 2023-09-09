f2a46f0b00f4b42c905852aab80c4220608eb09a
Dennis Capone <dennis.d.cap@gmail.com>
2023-09-08 21:44:18 -0400

    Fixed the query method from returning duplicates of entries.

--- mtgb.py
+++ mtgb.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from helpers import arena_deck_import_helper, mtgb_helper
 import logging
+import traceback
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -48,18 +49,23 @@ async def invesitgate_deck(ctx):
     
     '''
     try:
-        logger.info('Received deck file data')
-        await ctx.channel.send('Received {}'.format(ctx.message.attachments[0].filename))
+        logger.info('Received deck file data.\nGuild: {},Channel: {}'.format(ctx.guild.name,ctx.channel.name))
+        file_name = ctx.message.attachments[0].filename
+        await ctx.channel.send('======Received {}======'.format(file_name))
         await ctx.channel.send('Processing')
-        await ctx.channel.send('======')
+        thread = await ctx.channel.create_thread(
+            name=file_name,
+            type=discord.ChannelType.public_thread
+        )
         deck_str = (await ctx.message.attachments[0].read()).decode('utf-8')
         local_deck = arena_deck_import_helper.parse_deck_str(deck_str)
         card_list = local_deck.get_basic_card_list()
         for i in card_list:
-            await ctx.channel.send(i)
-            await ctx.channel.send('======')
-        await ctx.channel.send('Complete')
+            await thread.send(i)
+            await thread.send('======')
+        await ctx.channel.send('======Complete {}======'.format(ctx.message.author.mention))
     except Exception:
+        traceback.print_exc()
         logger.exception('Exception in investigate_deck()',stack_info=True)
         await ctx.channel.send('Error Occured')
 
@@ -87,27 +93,40 @@ async def get_card_data(ctx, *args):
         Variable number of flags and parameters provided via Discord message 
     '''
     try:
-        logger.info('Received card query')
-        await ctx.channel.send('Querying')
-        await ctx.channel.send('======')
+        logger.info('Received card query.\nGuild: {}, Channel: {}'.format(ctx.guild.name,ctx.channel.name))
+        await ctx.channel.send('======Querying======')
+        thread = await ctx.channel.create_thread(
+            name="{}'s Card Query".format(ctx.message.author),
+            type=discord.ChannelType.public_thread
+        )
+        flags_present = False
+        for i in args:
+            if '--' in i:
+                flags_present = True
+        if not flags_present:
+            await ctx.channel.send("======No flags passed. Please use `mtgb:help get-card-data` for info!======")
+            return
         parameters = mtgb_helper.extract_flags_and_params(argv=list(args))
         return_list = parameters[0]
         cards = mtgb_helper.find_card(parameters[1])
-        return_str = ''
-
+        print(len(cards))
+        if len(return_list) == 0:
+                return_list = ['name', 'set','mana_cost','type','text','image_url']
+        
+        return_messages = []
         for card in cards:
-            if len(return_list) == 0:
-                return_list = ['name', 'mana_cost','type','text','image_url']
-
             for item  in return_list:
-                return_str += '{}: {},\n'.format(item.capitalize().replace('_',' '), card.__getattribute__(item))
-            await ctx.channel.send(return_str[:-2])
-            await ctx.channel.send('======')
+                return_messages.append('{}: {},\n'.format(item.capitalize().replace('_',' '), card.__getattribute__(item)))
+            return_messages.append('======')
+        
+        for message in return_messages:
+            await thread.send(message)
+                
         if len(cards) == 0:
             await ctx.channel.send('No Card Found. Please check parameters.')
-            await ctx.channel.send('======')
-        await ctx.channel.send('Complete')
+        await ctx.channel.send('======Complete {}======'.format(ctx.message.author.mention))
     except Exception:
+        traceback.print_exc()
         logger.exception('Exception in investigate_deck()',stack_info=True)
         await ctx.channel.send('Error Occured')
     
